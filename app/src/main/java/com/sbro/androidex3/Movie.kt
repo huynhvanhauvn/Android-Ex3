5a0cc422a0a250ad94879267bba2dac5327bad08
Huynh Van Hau <hau.huynh-van@banvien.com.vn>
2020-05-09 12:45:47 +0700

    Initial commit

---
+++ app/src/main/java/com/sbro/androidex3/Movie.kt
@@ -0,0 +1,20 @@
+package com.sbro.androidex3
+
+public class Movie(
+    val popularity: Double,
+    val voteCount: Int,
+    val video: Boolean,
+    val posterPath: String,
+    val id: Int,
+    val adult: Boolean,
+    val backdropPath: String,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val genreIds: List<Int>,
+    val title: String,
+    val voteAverage: Double,
+    val overview: String,
+    val releaseDate: String
+) {
+
+}
