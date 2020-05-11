b84a922897e1ab6e9e734059e09a141f775c604e
lchao <laochihu@gmail.com>
2020-05-11 14:49:12 +0700

    Get image by url

--- app/src/main/java/com/sbro/androidex3/MovieAdapter.kt
+++ app/src/main/java/com/sbro/androidex3/MovieAdapter.kt
@@ -1,6 +1,9 @@
 package com.sbro.androidex3
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,22 +11,30 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import java.io.IOException
+import java.io.InputStream
 
 class MovieAdapter(moviesTitle : Array<String>,
                    moviesContent : Array<String>,
                    moviesImage : Array<Int>,
-                   ct : Context) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+                   imageLink : Array<String>,
+                   ct : Context,
+                   act : Activity) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
+    var moviesImageLink = listOf<String>()
     var moviesTitleList = listOf<String>()
     var moviesContentList = listOf<String>()
     var imageList = listOf<Int>()
     var context : Context? = null
+    var activity : Activity? = null
 
     init {
+        moviesImageLink = imageLink.toList()
         moviesTitleList = moviesTitle.toList()
         moviesContentList = moviesContent.toList()
         imageList = moviesImage.toList()
         context = ct
+        activity = act
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -55,9 +66,40 @@ class MovieAdapter(moviesTitle : Array<String>,
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(moviesTitleList.get(position))
         holder.title?.text = moviesTitleList.get(position)
         holder.content?.text = moviesContentList.get(position)
-        holder.image?.setImageResource(imageList[position])
-        holder.bind(moviesTitleList.get(position))
+        try{
+            NewThread(moviesImageLink.get(position), holder.image, activity).start()
+        }catch (e : IOException){
+            println(e.message)
+            holder.image?.setImageResource(R.drawable.ic_launcher_foreground)
+        }
+
+    }
+
+    private class NewThread(Url : String, imgView : ImageView?, activity : Activity?) : Thread() {
+        var activity : Activity? = null
+        var url : String? = null
+        var imgView : ImageView? = null
+
+        init {
+            this.url = Url
+            this.imgView = imgView
+            this.activity = activity
+        }
+
+        override fun run() {
+            try {
+                var bmp : Bitmap? = null
+                var input : InputStream = java.net.URL(url.toString()).openStream()
+                bmp = BitmapFactory.decodeStream(input)
+                this.activity?.runOnUiThread(java.lang.Runnable {
+                    this.imgView?.setImageBitmap(bmp)
+                })
+            }catch (e : Exception) {
+                println(e.message)
+            }
+        }
     }
 }
